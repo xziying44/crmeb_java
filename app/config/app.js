08d7b8d64653c38da24fee46e6e4c84d4e22428c
wangyanzhou <wangyanzhou@uguardsec.com>
2026-01-20 09:38:05 +0800

    切换到调试环境

--- app/config/app.js
+++ app/config/app.js
@@ -8,7 +8,7 @@
 // | Author: CRMEB Team <admin@crmeb.com>
 // +----------------------------------------------------------------------
 //移动端商城API
-let domain = 'http://192.168.31.35:2511'
+let domain = 'http://127.0.0.1:20510'
 // let domain = 'https://apif.java.crmeb.net'
 
 module.exports = {
